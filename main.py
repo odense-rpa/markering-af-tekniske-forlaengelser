49ea8b471d2e1136b3c3bfe24a33fc93a5f4d70b
Jakob Kirkegaard Wibroe <jkwibroe91@gmail.com>
2025-09-04 10:26:21 +0200

    skiftet creds til ATS

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import asyncio
 import logging
 import sys
-from dotenv import load_dotenv
-import os
 import datetime
 
 from automation_server_client import AutomationServer, Workqueue, WorkItemError, Credential
 from momentum_client.manager import MomentumClientManager
 from odk_tools.tracking import Tracker
 
-load_dotenv()  # Load environment variables from .env file
 
 tracker: Tracker
 momentum: MomentumClientManager
@@ -113,14 +110,15 @@ if __name__ == "__main__":
     tracker = Tracker(
         username=tracking_credential.username, password=tracking_credential.password
     )
-
+    momentum_credential = Credential.get_credential("Momentum - produktion")
     momentum = MomentumClientManager(
-    base_url=os.getenv("BASE_URL"),
-    client_id=os.getenv("CLIENT_ID"),
-    client_secret=os.getenv("CLIENT_SECRET"),
-    api_key=os.getenv("API_KEY"),
-    resource=os.getenv("RESOURCE"),
-)
+        base_url=momentum_credential.data["base_url"],
+        client_id=momentum_credential.username,
+        client_secret=momentum_credential.password,
+        api_key=momentum_credential.data["api_key"],
+        resource=momentum_credential.data["resource"],
+    )
+
 
     # Queue management
     if "--queue" in sys.argv:
